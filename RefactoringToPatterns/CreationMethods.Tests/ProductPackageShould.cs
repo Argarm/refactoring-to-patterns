83ea18c40128001bb8290eb2d67b4b75575d4b31
Aaron García <aargarcia@aidacanarias.com>
2022-11-09 21:53:26 +0000

    move builder to his own file

--- RefactoringToPatterns/CreationMethods.Tests/ProductPackageShould.cs
+++ RefactoringToPatterns/CreationMethods.Tests/ProductPackageShould.cs
@@ -53,29 +53,4 @@ namespace RefactoringToPatterns.CreationMethods.Tests
             Assert.True(productPackage.HasTv());
         }
     }
-
-    public class PackageBuilder {
-        private string internetLabel;
-        private int? voip;
-        private string[] tvChannels;
-
-        public PackageBuilder WithInternet(string internetLabel) {
-            this.internetLabel = internetLabel;
-            return this;
-        }
-
-        public PackageBuilder WithVoip(int voip) {
-            this.voip = voip;
-            return this;
-        }
-
-        public PackageBuilder WithTvChannels(string[] tvChannels) {
-            this.tvChannels = tvChannels;
-            return this;
-        }
-
-        public ProductPackage Build() {
-            return ProductPackage.CreatePackage(internetLabel,voip,tvChannels);
-        }
-    }
 }
